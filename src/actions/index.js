8e6580feab9eeff8d36fe320c62d179a1184fc60
bertin <ruhimbazab@gmail.com>
2021-02-01 23:13:14 -0800

    stream-form
    adding streamForm file in components
    refactoring streamCreate and streamEdit file
    
    form

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import formValues from 'redux-form/lib/formValues';
+import history from '../history';
 import streams from '../apis/streams';
 import { 
     SIGN_IN, 
@@ -30,6 +30,7 @@ export const createStream = formValues =>async (dispatch, getState) => {
    const response = await streams.post('/streams', {...formValues, userId});
 
    dispatch({type: CREATE_STREAM, payload: response.data});
+   history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -45,9 +46,10 @@ export const fetchStream = (id) => async (dispatch) => {
 }
 
 export const editStream = (id, formValues) => async (dispatch) => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({type: EDIT_STREAM, payload: response.data});
+    history.push('/');
 }
 
 export const deleteStream = (id) => async (dispatch) => {
--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamCreate from './Streams/StreamCreate';
 import StreamEdit from './Streams/StreamEdit';
 import StreamDelete from './Streams/StreamDelete';
 import StreamList from './Streams/StreamList';
 import StreamShow from './Streams/StreamShow';
 import Header     from './Header';
+import history    from '../history';
 
 
 const App = () => {
 
     return(
         <div className="ui container">
-            <BrowserRouter>
+            <Router history={history}>
             <Header />
             <div>
                 <Route path="/" exact component={StreamList} />
                 <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/edit/:id" exact component={StreamEdit} />
                 <Route path="/streams/delete" exact component={StreamDelete} />
                 <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
             </div>
     );
 }
